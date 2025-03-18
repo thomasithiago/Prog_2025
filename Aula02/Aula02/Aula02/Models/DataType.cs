af777befc871614ffa9bef882c5eff9b621aaad8
Thiago Thomasi <156266186+thomasithiago@users.noreply.github.com>
2025-03-17 21:17:01 -0300

    Aula02

---
+++ Aula02/Aula02/Aula02/Models/DataType.cs
@@ -0,0 +1,47 @@
+﻿namespace Aula02.Models
+{
+    public class DataType
+    {
+        public char myVar = 'a';    
+        public char myConst = 'b';
+
+        public char myChar1 = 'f';
+        public char myChar2 = ':';
+        public char myChar3 = 'x';
+
+        //Podemos também atribuir refência
+        //de caracteres Unicode
+
+        public char myChar4 = '\u2726';
+
+        //Podemos aainda mesclar caracteres especiais 
+        //Como, nova linha, tabulação e etc.
+        public string textLine = "Esta é uma linha de texto.\n\n\n";
+
+        /*
+          \e =Caracter de escape
+           \n =Nova Linha
+            \r =Retorno
+             \t =Tabulação Horizontal 
+              \"=Aspas duplas,usado para exibir dentro de string
+               \'=Aspas simples, usado para exibir aaspas simples dentro de string  
+         */
+
+       
+        private int count = 10;
+        public string message;
+        public DataType()
+        {    // Interpolação de Strings 
+             // Combinando string de diferente maneiras no C#
+            message = $"o contador está em {count}";
+
+            string username = "Thiago";
+            int inboxCount = 10;
+            int maxCount = 100;
+
+            message += $"\n o usuario {username} tem {inboxCount } Mensagens";
+            message += $"\nespaço restante na sua caixa {maxCount - inboxCount}";
+        }
+      
+    }
+}
---
+++ Aula02/Aula02/Aula02/Models/TypeCasting.cs
@@ -0,0 +1,31 @@
+﻿namespace Aula02.Models
+{
+    public class TypeCasting
+    {
+        //Declarando variavéis na classe
+        public int myInteger = 20;
+        public long myLong;
+
+        public string myType1;
+        public string myType2;
+        public TypeCasting()
+        {
+            //Conversão implicita de tipos 
+            myLong = myInteger;
+            //myInteger = myLong;
+            //Neste caso o long é muito grande para para o int, e a conversão implicita não é permitida
+
+            //Conversão explicita 
+            long myLong2 = 138129210;
+            int myInteger2;
+            myInteger2 = (int)myLong2;
+
+            //É Possivel identificar qual é o tipo de uma variavel em tempo de execução
+            myType1 = myLong2.GetType(). ToString();   
+            myType2 = myInteger2.GetType(). ToString();
+
+
+        }
+
+    }
+}
