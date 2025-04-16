d4e059d54e3bb21fb2660e1c30eddb098ad106b9
Thiago Thomasi <156266186+thomasithiago@users.noreply.github.com>
2025-04-16 08:15:31 -0300

    AtividadeRecursividade

---
+++ Recursividade/AtividadeRecursividade/AtividadeRecursividade/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AtividadeRecursividade.Models;
+
+namespace AtividadeRecursividade.Controllers;
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        
+    
+    
+    
+    
+
+    
+    
+    
+    
+
+   
+    
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
