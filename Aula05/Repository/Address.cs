fc95818291e9c8a4fc1ee251b0468d9ed3fbc5cc
Thiago Thomasi <156266186+thomasithiago@users.noreply.github.com>
2025-06-04 11:28:09 -0300

    Update Address.cs

--- Aula05/Repository/Address.cs
+++ Aula05/Repository/Address.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repository
+namespace Modelo
 {
-    class Address
+    public class Address
     {
+        public int Id { get; set; }
+        public string? Street1 { get; set; }
+        public string? Street2 { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+        public string? AddressType { get; set; }
     }
 }
